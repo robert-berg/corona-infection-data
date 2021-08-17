475fdae95cc97b84127edfd44850bb9fd0f0c86d
Robert Berg <r.berg.2001@gmail.com>
2021-08-17 08:05:03 +0200

    first commit

---
+++ PopulationEntry.java
@@ -0,0 +1,22 @@
+public class PopulationEntry {
+
+    private String code;
+    private double population;
+
+    public PopulationEntry(String code, double population) {
+
+        this.code = code;
+        this.population = population;
+
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public double getPopulation() {
+        return population;
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Java program for visualizing Corona infection data
+![](/output/DE.png)
+<ul>
+    <li>Generates PNG files with graphs based on the official WHO data</li>
+    <li>Countries can be chosen by editing the country codes in Chart.java</li>
+</ul>
